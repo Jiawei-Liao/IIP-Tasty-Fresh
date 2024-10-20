f8b372e2bce91e209d5a2432c0d4d7be6b6fb7af
Jiawei Liao <jiaweiliao01@gmail.com>
2024-10-20 12:54:19 +1100

    dockerised script, added live camera and argument functionality, corrected product list lookup

--- detect.py
+++ detect.py
@@ -7,9 +7,10 @@ from datetime import datetime
 from pylibdmtx.pylibdmtx import decode
 import pandas as pd
 import cv2
-from typing import List, Tuple, Dict
+from typing import Tuple, Dict
+import argparse
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+# os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 # Load models
 general_model_path = 'general_model.pt'
@@ -21,7 +22,9 @@ sandwich_model = YOLO(sandwich_model_path)
 # Load lookup table
 product_list = pd.read_excel('product_list.xlsx')
 
-def process_sandwich(sandwich_image: Image) -> Dict[str, float]:
+general_items = ['SANDWICH', 'BURRITO']
+
+def process_sandwich(sandwich_image: Image.Image) -> Tuple[str, float]:
     '''
     Runs sandwich classifier on an image
     input: 
@@ -39,14 +42,11 @@ def process_sandwich(sandwich_image: Image) -> Dict[str, float]:
         top_class_confidence = sandwich_results[0].probs.top1conf.item()
         sandwich_name = sandwich_results[0].names[top_class_index]
 
-        return {
-            "item": sandwich_name,
-            "confidence": top_class_confidence
-        }
+        return (sandwich_name, top_class_confidence)
     else:
         return None
 
-def process_data_matrix(data_matrix_image: Image) -> str:
+def process_data_matrix(data_matrix_image: Image.Image) -> str:
     '''
     Runs data matrix decoder on an image
     input:
@@ -57,9 +57,22 @@ def process_data_matrix(data_matrix_image: Image) -> str:
 
     # decode the data matrix
     result = decode(data_matrix_image)
-    return str(result[0]).split("'") if result else None
+    return str(result[0]).split('\'') if result else None
 
-def validate(item: Tuple[Dict, Image]) -> Dict:
+def user_input(image: Image.Image) -> str:
+    '''
+    Prompts user to input the item name
+    input:
+        image: image of the item
+    output:
+        item name
+    '''
+
+    image.show()
+    print('Could not identify item, please enter the item name:')
+    return input()
+
+def validate(item_obj: Tuple[Dict, Image.Image]) -> Dict:
     '''
     Validates an item, prompting user to correct if necessary
     input: 
@@ -67,41 +80,64 @@ def validate(item: Tuple[Dict, Image]) -> Dict:
     output:
         Tuple of validated item JSON object and image
     '''
-    image = item[1]
-    item = item[0]
+    item = item_obj[0]
+    image = item_obj[1]
 
-    # Check for if item is "SANDWICH", if so, it is not correctly classified and requires user input
-    if item['item'] == "SANDWICH":
-        image.show()
-
-        print("Could not identify sandwich, please enter the sandwich name:")
-        item['item'] = input()
-        item['user_input'] = True
-
-        row = product_list[product_list['ProductDescription'] == item['item']]
-        if not row.empty:
-            item['SKU'] = row['SKU'].values[0]
-            item['barcode'] = row['barcode'].values[0]
-            item['lookupcode'] = row['lookupcode'].values[0]
-    # Check if item matches barcode
+    # Check for if item is 'SANDWICH', if so, it is not correctly classified and requires user input
+    if item['item'] in general_items:
+        # If both sandwich item and data matrix is found
+        if (item['sandwich_item'] and item['data_matrix']):
+            barcode = item['data_matrix']
+            row = product_list[product_list['barcode'] == barcode]
+            if not row.empty:
+                data_matrix_item = product_list[item['data_matrix']].values[0]
+                # Check if they match
+                if data_matrix_item == item['sandwich_item']:
+                    item['item'] = item['sandwich_item']
+                # Inconsistency found, prompt user to correct
+                else:
+                    item['item'] = user_input(image)
+                    item['user_input'] = True
+            else:
+                print('${barcode} not found in lookup table')
+        # If only sandwich or data matrix is found, use that information
+        elif item['sandwich_item']:
+            item['item'] = item['sandwich_item']
+        elif item['data_matrix']:
+            item['item'] = product_list[item['data_matrix']].values[0]
+        # If neither is found, prompt user to correct
+        else:
+            item['item'] = user_input(image)
+            item['user_input'] = True
+    
+    # Check if item matches barcode from data matrix
     elif item['data_matrix'] is not None:
-        barcode = item['data_matrix'][1]
+        barcode = item['data_matrix']
         row = product_list[product_list['barcode'] == barcode]
         if not row.empty:
-            item['SKU'] = row['SKU'].values[0]
-            item['barcode'] = row['barcode'].values[0]
-            item['lookupcode'] = row['lookupcode'].values[0]
+            data_matrix_item = product_list[item['data_matrix']].values[0]
+            # Check if item and data matrix item match
+            if data_matrix_item == item['item']:
+                item['item'] = item['data_matrix']
+            # Inconsistency found, prompt user to correct
+            else:
+                item['item'] = user_input(image)
+                item['user_input'] = True
         else:
-            img = item[1]
-            img.show()
+            print('${barcode} not found in lookup table')
 
-            print("Barcode does not match any product, please enter the product name:")
-            item['item'] = input()
-            item['user_input'] = True
+    # Fetch SKU, barcode, and lookup code from lookup table
+    row = product_list[product_list['ProductDescription'] == item['item']]
+    if not row.empty:
+        item['SKU'] = str(row['SKU'].values[0])
+        item['barcode'] = str(row['barcode'].values[0])
+        item['lookupcode'] = str(row['lookupcode'].values[0])
+    else:
+        print('Product ${product} not found in lookup table')
     
     return item
     
-def process_image(image: Image) -> Dict:
+def process_image(image: Image.Image) -> Dict:
     '''
     Processes an image, running it through the general model, sandwich classifier, and data matrix decoder
     input: 
@@ -122,28 +158,28 @@ def process_image(image: Image) -> Dict:
         # (Item JSON object, Image)
         item = (
             {
-                "item": item_name,
-                "confidence": confidence,
-                "sandwich_confidence": None,
-                "data_matrix": None,
-                "SKU": None,
-                "barcode": None,
-                "lookupcode": None,
-                "user_input": False
+                'item': item_name,
+                'confidence': confidence,
+                'sandwich_item': None,
+                'sandwich_confidence': None,
+                'data_matrix': None,
+                'SKU': None,
+                'barcode': None,
+                'lookupcode': None,
+                'user_input': False
             },
             item_image
         )
 
         # If the item is a sandwich, crop the image and run it through the sandwich classifier
-        if item_name == "SANDWICH":
-            sandwich_result = process_sandwich(item_image)
-            if sandwich_result is not None:
-                item[0]['item'] = sandwich_result["item"]
-                item[0]['sandwich_confidence'] = sandwich_result["confidence"]
+        if item_name == 'SANDWICH':
+            sandwich_item, sandwich_confidence = process_sandwich(item_image)
+            if sandwich_item is not None:
+                item[0]['sandwich_item'] = sandwich_item
+                item[0]['sandwich_confidence'] = sandwich_confidence
         
         # Run the data matrix detector
         data_matrix_result = process_data_matrix(item_image)
-
         item[0]['data_matrix'] = data_matrix_result
 
         items.append(item)
@@ -156,53 +192,86 @@ def process_image(image: Image) -> Dict:
     
     # Create the final JSON object
     result = {
-        "card_id": "1",
-        "date_created": datetime.now().isoformat(),
-        "items": items
+        'card_id': '1',
+        'date_created': datetime.now().isoformat(),
+        'items': items
     }
 
     return result
 
 def main():
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Error: Could not open camera.")
-        return
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='IIP Detect')
+
+    parser.add_argument('--use_cam', type=lambda x: (str(x).lower() == 'true'), default=False,
+                    help='Enable or disable camera usage (True/False). Default is False.')
+    parser.add_argument('--img_path', type=str, default=None,
+                    help='Path to image to process. Default is None.')
     
-    image = None
+    args = parser.parse_args()
 
-    try:
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                print("Error: Could not read frame.")
-                break
+    useCamera = args.use_cam
+    img_path = args.img_path
 
-            image = frame
+    # Live input
+    if useCamera:
+        cap = cv2.VideoCapture(1)
+        if not cap.isOpened():
+            print('Error: Could not open camera.')
+            return
+        
+        image = None
 
-            cv2.imshow('frame', frame)
+        try:
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    print('Error: Could not read frame.')
+                    break
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                print("Finding items...")
-                break
-    finally:
-        cap.release()
-        cv2.destroyAllWindows
+                image = frame
 
-        if image is not None:
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            result = process_image(Image.fromarray(image))
+                cv2.imshow('frame', frame)
 
-            if result is None:
-                print("No items found.")
-                return
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    print('Finding items...')
+                    break
+        finally:
+            cap.release()
+            cv2.destroyAllWindows
+
+            if image is not None:
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                result = process_image(Image.fromarray(image))
+
+                if result is None:
+                    print('No items found.')
+                    return
+                
+                print(json.dumps(result, indent=4))
+
+                with open('result.json', 'w') as f:
+                    json.dump(result, f, indent=4)
+    # Image input
+    else:
+        if img_path is None:
+            img_path = 'demo/2.png'
+        else:
+            img_path = os.path.join('demo', img_path)
+        
+        image = Image.open(img_path)
+        result = process_image(image)
+
+        if result is None:
+            print('No items found.')
+            return
+        
+        print(json.dumps(result, indent=4))
+
+        with open('result.json', 'w') as f:
+            json.dump(result, f, indent=4)
             
-            print(json.dumps(result, indent=4))
-
-            with open('result.json', 'w') as f:
-                json.dump(result, f, indent=4)
-
 if __name__ == '__main__':
     start_time = time.time()
     main()
-    print(f"Time taken: {time.time() - start_time:.2f}s")
+    print(f'Time taken: {time.time() - start_time:.2f}s')
